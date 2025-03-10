733b3425b579a0b8b64afc8982ce092ac8301a05
Ben Ridings <benridings@hotmail.com>
2025-03-10 19:58:26 +0000

    Add word model

--- wordle_backend/wordle_api/models.py
+++ wordle_backend/wordle_api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Word(models.Model):
+    word = models.CharField(max_length=5, unique=True)
+
+    def __str__(self):
+        return self.word
