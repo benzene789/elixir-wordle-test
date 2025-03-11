2e3f0858ba22b9ac6d7e61d078d17814709ff00a
Ben Ridings <benridings@hotmail.com>
2025-03-11 20:57:42 +0000

    Reduce line length

--- wordle_backend/populate_words.py
+++ wordle_backend/populate_words.py
@@ -6,7 +6,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "wordle_backend.settings")
 django.setup()
 
 
-words = ["apple", "brave", "crane", "dwarf", "elite", "fairy", "grape", "honey", "igloo", "jolly"]
+words = ["apple", "brave", "crane", "dwarf",
+         "elite", "fairy", "grape", "honey",
+         "igloo", "jolly"]
 
 for word in words:
     Word.objects.create(word=word)
